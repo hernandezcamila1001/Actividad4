fd52f18778d562746cbdec663dfbe51fd1a6ae40
hernandezcamila1001 <143672258+hernandezcamila1001@users.noreply.github.com>
2023-09-10 21:13:51 -0500

    Update baseDatos.py

--- BD/baseDatos.py
+++ BD/baseDatos.py
@@ -1,10 +1,8 @@
-""" Se importa la librería MongoClient que permite establecer una conexión con el servidor MongoDB. """
 from pymongo.mongo_client import MongoClient
 
-"""Conexión con la base de datos."""
-def conexion(): # Se crea la función "conexion" que establece y devuelve una conexión con los datos de MongoDB.
-    uri = "mongodb+srv://hernandezcamila1001:1234@cluster0.pn7altv.mongodb.net/?retryWrites=true&w=majority" # URI de conexión.
-    return MongoClient(uri) # Se retorna la conexión que se crea con la base de datos de MongoDB.
+def conexion(): 
+    uri = "mongodb+srv://hernandezcamila1001:1234@cluster0.pn7altv.mongodb.net/?retryWrites=true&w=majority"
+    return MongoClient(uri) 
 
 #READ
 """ Código necesario para crear un read en MongoDB"""
