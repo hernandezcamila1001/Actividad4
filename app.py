5e653acb4dccccdd6ccea31dff2b84fc3eb777ba
hernandezcamila1001 <143672258+hernandezcamila1001@users.noreply.github.com>
2023-09-10 22:01:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,29 @@ import pandas as pd # Se importa la biblioteca "pandas" que se encarga del anál
 """ Análisis de datos (Sensor 1) """
 
 """ Se llama a la función “dataTeorico()” que comprende dos valores, los cuales van a ser asignados a las variables 
-"dataTeoricoEsfuerzo" y "dataTeoricoDeformacion", donde la infomación viene siendo importada de "Data.data". """
+"dataTeoricoEsfuerzo" y "dataTeoricoDeformacion", donde la infomación es importada de "Data.data". """
 dataTeoricoEsfuerzo, dataTeoricoDeformacion = dataTeorico() 
 
 """ Modelo de regresión """
 
 data_list = lecturaDatos() # Se define la variable "data_list" que comprende la función "lecturaDatos(), llamada desde "BD.baseDatos".
 data_real = pd.DataFrame(data_list) # Se define la variable "data_real" que crea una base de datos de la información suministrada en la variable "data_list".
-data_real_fit = data_real
+data_real_fit = data_real # Se crea una variable "data_real_fi" que contiene la información de "data_real".
+
+""" Se define la variable "X" con los datos de la columna 'Esfuerzo (kN)' de "data_real_fit", donde cada uno de 
+estos se presentarán como una lista de números. """
 X = data_real_fit['Esfuerzo (kN)'].values.reshape(-1, 1)
+""" Se define la variable "y" con los datos de la columna 'Deformación (mm)' de "data_real_fit", donde cada uno de 
+estos se presentarán como una lista de números. """
 y = data_real_fit['Deformación (mm)'].values.reshape(-1, 1)
-x_lim = data_real['Esfuerzo (kN)'].iloc[-1]
-y_lim = data_real['Deformación (mm)'].iloc[-1]
-model = LinearRegression()
-model.fit(X, y)
-prediction = round(model.predict(np.array([3000]).reshape(-1, 1))[0][0],1)
-print('La predicción a 3000 kN es de: ', prediction ,'mm')
+x_lim = data_real['Esfuerzo (kN)'].iloc[-1] # Se define la variable "x_lim" donde se elecciona el último valor de la columna 'Esfuerzo (kN)', y se guarda.
+y_lim = data_real['Deformación (mm)'].iloc[-1] # Se define la variable "y_lim" donde se elecciona el último valor de la columna 'Deformación (mm)', y se guarda.
+
+
+model = LinearRegression() # Se define la variable "model" que proporciona el modelo de la regresión lineal.
+model.fit(X, y) # Se entrena el modelo para lograr obtener el mejor ajuste a los datos proporcionados en "X" y "y".
+prediction = round(model.predict(np.array([3000]).reshape(-1, 1))[0][0],1) # Se define la variable "prediction" donde se obtiene la predicción del modelo para el valor de 3000.
+print('La predicción a 3000 kN es de: ', prediction ,'mm') # Se visualiza el resultado de la predicción.
 
 
 dataRealEsfuerzo = data_real['Esfuerzo (kN)']
